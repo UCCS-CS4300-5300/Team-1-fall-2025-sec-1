2d3e2dadc8f3d3b03f349cd482b775081091ac9a
NinetimexX04 <alex.van.der.merwe.3200@gmail.com>
2025-10-06 15:05:46 -0600

    Landing page

--- GroupThink/GroupThink/settings.py
+++ GroupThink/GroupThink/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+# git test
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,7 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'app-brackencontainer-19.devedu.io',
-    'editor-brackencontainer-19.devedu.io'
+    'editor-brackencontainer-19.devedu.io',
+    '127.0.0.1'
 ]
 
 
