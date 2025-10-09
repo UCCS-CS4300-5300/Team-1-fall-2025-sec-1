bce78ec180e5c257d78c233778742c0b97094641
bbrinkel <brackenb96@gmail.com>
2025-10-09 07:12:14 +0000

    Got the very basics of Jitsi running, but meetings don't work because they require log-in through Jitsi. Currently using the public Jitsi meet service, wont work for this project. Will change to JaaS in the next push.

--- GroupThink/home/urls.py
+++ GroupThink/home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import index
+from . import views
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', views.index, name='index'),
+    path('create_meeting/', views.create_meeting, name='create_meeting'),
+    path('join_meeting/<str:room_name>/', views.join_meeting, name='join_meeting'),
 ]
--- GroupThink/home/views.py
+++ GroupThink/home/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Meeting
+import uuid
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'home/index.html')
+
+def create_meeting(request):
+
+    if request.method == 'POST':
+        meeting = Meeting.objects.create(title=request.POST.get('title'), 
+            room_name=f"groupthink-{uuid.uuid4().hex[:10]}")
+        return redirect('join_meeting', room_name=meeting.room_name)
+
+    return render(request, 'home/create_meeting.html')
+
+def join_meeting(request, room_name):
+    meeting = Meeting.objects.get(room_name=room_name)
+
+    return render(request, 'home/join_meeting.html', {'meeting': meeting})
+
