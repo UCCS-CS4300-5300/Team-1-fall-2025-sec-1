edb1f372be8c70f94518b89a26649eb878dbc1a0
NinetimexX04 <alex.van.der.merwe.3200@gmail.com>
2025-10-19 17:13:29 -0600

    Fixed STATIC_ROOT

--- GroupThink/GroupThink/settings.py
+++ GroupThink/GroupThink/settings.py
@@ -124,13 +124,20 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"   # Required for collectstatic on Render
+
+# Optional: serve extra local static assets
+# STATICFILES_DIRS = [BASE_DIR / "static"]
+
+# Enable WhiteNoise for serving static files in production
+MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
+
+# Optional, but improves static file caching/compression
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
