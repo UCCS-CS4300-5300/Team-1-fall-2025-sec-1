6dc59c9b46a6b88e9264ee2f6725d1ea0d807bb5
Antoine Cedotal <antoine.cedotal@gmail.com>
2025-11-05 17:26:37 +0000

    Fixed trusted origin of 'acedcontainer' to include https

--- GroupThink/GroupThink/settings.py
+++ GroupThink/GroupThink/settings.py
@@ -40,7 +40,7 @@ ALLOWED_HOSTS = [
 CSRF_TRUSTED_ORIGINS = [
     "https://groupthink-gt.onrender.com",
     "https://app-brackencontainer-19.devedu.io",
-    "http://app-acedcontainer-19.devedu.io/"
+    "https://app-acedcontainer-19.devedu.io"
 ]
 
 
