137f39b655d4ca0168b435fb171425e1908482cb
NinetimexX04 <alex.van.der.merwe.3200@gmail.com>
2025-12-06 17:53:49 -0700

    Update asgi.py

--- GroupThink/GroupThink/asgi.py
+++ GroupThink/GroupThink/asgi.py
@@ -1,6 +1,11 @@
 import os
 import django
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "GroupThink.settings")
+
+# Set up Django
+django.setup()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
@@ -8,12 +13,6 @@ from django.core.asgi import get_asgi_application
 import GroupThink.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "GroupThink.settings")
-
-# Set up Django
-django.setup()
-
-# Standard Django ASGI application for HTTP
-django_asgi_app = get_asgi_application()
 
 # Main ASGI application
 application = ProtocolTypeRouter(
